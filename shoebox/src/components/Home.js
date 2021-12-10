d2cd461cf0f694b70aa8542c80b22d69a7863dd6
surya1701 <suryavardan17@gmail.com>
2021-12-10 21:32:32 +0530

    Updated Cart & Home

--- shoebox/src/components/Home.js
+++ shoebox/src/components/Home.js
@@ -27,6 +27,9 @@ function Home({cartValue}) {
                  className={"btn btn-primary btn-sm mr-2 mb-1"}>
                     Nike<PlusCircleIcon width={"20px"}/>
                  </button>
+                 <br/>
+                <Advert className='img-fluid ad-image' src="/images/adidas-ad-image.jpg" />
+                <Advert_gif className='img-fluid' src="/images/adidas-ad.gif" />
             </Container>
         </div>
     )
@@ -51,3 +54,19 @@ const Container = styled.main`
         z-index: -1;
     }
 `
+const Advert = styled.img`
+position: absolute;
+left: 0;
+    &:hover {
+        opacity: 0;
+    }
+`;
+const Advert_gif = styled.img`
+opacity: 0;
+position: absolute;
+left: 0;
+z-index: 2;
+    &:hover {
+        opacity: 1;
+    }
+`;
