7280a4eac240969c6e7a517d5ea39b061c3733cb
surya1701 <suryavardan17@gmail.com>
2022-01-08 15:48:35 +0530

    Update Coupon.js

--- shoebox/src/features/cart/Coupon.js
+++ shoebox/src/features/cart/Coupon.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Grid, Typography} from "@material-ui/core";
+import {Button, Grid } from "@material-ui/core";
 import { useForm, FormProvider } from "react-hook-form";
 import FormInput from "../checkout/Forms/FormField";
 
