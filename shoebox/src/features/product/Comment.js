6b133981ad7277ad21c650e80c015e9a9f307bd3
surya1701 <suryavardan17@gmail.com>
2022-01-09 18:33:51 +0530

    Comment Section

--- shoebox/src/features/product/Comment.js
+++ shoebox/src/features/product/Comment.js
@@ -3,7 +3,7 @@ import {Button, Grid } from "@material-ui/core";
 import { useForm, FormProvider } from "react-hook-form";
 import FormInput from "../checkout/Forms/FormField";
 
-const Comment = ({ handleCoupon, user }) => {
+const Comment = ({ handleComment, user }) => {
 
     const methods = useForm();
 
@@ -12,7 +12,7 @@ const Comment = ({ handleCoupon, user }) => {
       <FormProvider {...methods}>
         <form
           onSubmit={methods.handleSubmit((data) =>
-            handleCoupon({...data})
+            handleComment({...data})
           )}
         >
           <Grid container spacing={3}>
